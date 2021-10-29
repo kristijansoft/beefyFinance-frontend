49ef2bf3cb19b612d7c29d54ad48a37c9b251809
kristijansoft <kristijan.pe99@gmail.com>
2021-10-29 12:55:38 -0500

    Make filter page

---
+++ src/app/components/Dropdown/Dropdown.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import "./Dropdown.scss";
+
+const Dropdown = ({ list }) => {
+  const [isOpen, setOpen] = useState(false);
+  const [items, setItem] = useState(list);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const toggleDropdown = () => setOpen(!isOpen);
+
+  const handleItemClick = (id) => {
+    selectedItem == id ? setSelectedItem(null) : setSelectedItem(id);
+  };
+
+  return (
+    <div className="dropdown">
+      <div className="dropdown-header" onClick={toggleDropdown}>
+        {selectedItem
+          ? items.find((item) => item.id == selectedItem).label
+          : "Select your destination"}
+        <i className={`fa fa-chevron-right icon ${isOpen && "open"}`}></i>
+      </div>
+      <div className={`dropdown-body ${isOpen && "open"}`}>
+        {items.map((item) => (
+          <div
+            className="dropdown-item"
+            onClick={(e) => handleItemClick(e.target.id)}
+            id={item.id}
+          >
+            <span
+              className={`dropdown-item-dot ${
+                item.id == selectedItem && "selected"
+              }`}
+            >
+              •{" "}
+            </span>
+            {item.label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
