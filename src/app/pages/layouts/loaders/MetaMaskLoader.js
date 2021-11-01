95d3b040f0f90f48f9d8c73022ecb1d985876263
yaroslav-io <yaroslav.bura7io@gmail.com>
2021-10-31 19:47:19 -0500

    update metamask win

---
+++ src/app/pages/layouts/loaders/MetaMaskLoader.js
@@ -0,0 +1,20 @@
+import { makeStyles } from "@material-ui/core";
+import React from "react";
+
+import { ReactComponent as MetaMaskImg } from "assets/images/metamask.svg";
+import "./MetaMaskLoader.scss";
+
+const MetaMaskLoader = () => {
+  return (
+    <div className="loader-container">
+      <div className="metaicon-wrapper">
+        <MetaMaskImg width="150" height="150" />
+      </div>
+      <div className="progress-bar">
+        <div className="circle border"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MetaMaskLoader;
