e799081f797010e695b7dfbff32d1dbaf5075c54
kristijansoft <kristijan.pe99@gmail.com>
2021-11-02 12:01:45 -0500

    font bigger

--- src/app/components/Logo/Logo.js
+++ src/app/components/Logo/Logo.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./Logo.scss";
-import LogoImg from "../../../assets/images/logo.svg";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './Logo.scss';
+import LogoImg from '../../../assets/images/logo.svg';
+import { Link } from 'react-router-dom';
 
 const Logo = ({ notext }) => {
   return (
     <div className="logo-wrapper">
       <Link to="/">
         <img src={LogoImg} alt="Beefy Finance" />
-        {!notext && <span>Beefy.Finance </span>}
+        {!notext && <span>beefy.finance </span>}
       </Link>
     </div>
   );
