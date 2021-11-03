d61696a366c9171c9b3ee81dcaba85300e64cbe3
yaroslav-io <yaroslav.bura7io@gmail.com>
2021-11-03 15:46:25 +0200

    update header font

--- src/app/components/Background/Background.js
+++ src/app/components/Background/Background.js
@@ -1,45 +1,47 @@
-import React from "react";
-import "./Background.scss";
+import React from 'react';
+import './Background.scss';
 
-const Background = () => {
+const Background = ({ showCows = true }) => {
   return (
     <div className="plains unselectable">
       <div className="plain-left"></div>
       <div className="plain-center-bg"></div>
       <div className="plain-right"></div>
       <div className="plain-center-fg"></div>
-      <div className="cows">
-        <img
-          className="cow"
-          src="https://www.beefy.finance/img/beefy.svg"
-          alt="cow"
-        />
-        <img
-          className="cow"
-          src="https://www.beefy.finance/img/beefy.svg"
-          alt="cow"
-        />
-        <img
-          className="cow"
-          src="https://www.beefy.finance/img/beefy.svg"
-          alt="cow"
-        />
-        <img
-          className="cow"
-          src="https://www.beefy.finance/img/beefy.svg"
-          alt="cow"
-        />
-        <img
-          className="cow"
-          src="https://www.beefy.finance/img/beefy.svg"
-          alt="cow"
-        />
-        <img
-          className="cow"
-          src="https://www.beefy.finance/img/beefy.svg"
-          alt="cow"
-        />
-      </div>
+      {showCows && (
+        <div className="cows">
+          <img
+            className="cow"
+            src="https://www.beefy.finance/img/beefy.svg"
+            alt="cow"
+          />
+          <img
+            className="cow"
+            src="https://www.beefy.finance/img/beefy.svg"
+            alt="cow"
+          />
+          <img
+            className="cow"
+            src="https://www.beefy.finance/img/beefy.svg"
+            alt="cow"
+          />
+          <img
+            className="cow"
+            src="https://www.beefy.finance/img/beefy.svg"
+            alt="cow"
+          />
+          <img
+            className="cow"
+            src="https://www.beefy.finance/img/beefy.svg"
+            alt="cow"
+          />
+          <img
+            className="cow"
+            src="https://www.beefy.finance/img/beefy.svg"
+            alt="cow"
+          />
+        </div>
+      )}
     </div>
   );
 };
