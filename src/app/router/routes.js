033a3d5e90f772a1f28f217b3c22565f89ab208f
kristijansoft <kristijan.pe99@gmail.com>
2021-11-05 08:05:02 -0500

    update modal

--- src/app/router/routes.js
+++ src/app/router/routes.js
@@ -1,73 +1,89 @@
-import React from "react";
-import loadable from "@loadable/component";
-import Loader from "../pages/layouts/client/Loader";
-import MetaMaskLoader from "app/pages/layouts/loaders/MetaMaskLoader";
+import React from 'react';
+import loadable from '@loadable/component';
+
+import Loader from '../pages/layouts/client/Loader';
+import MetaMaskLoader from 'app/pages/layouts/loaders/MetaMaskLoader';
+
+// function Loading(props) {
+//   if (props.pastDelay) {
+//     return <MetaMaskLoader />;
+//   } else {
+//     return null;
+//   }
+// }
+function Loading(props) {
+  if (props.pastDelay) {
+    return <div>Loading...</div>;
+  } else {
+    return <div></div>;
+  }
+}
 
 const routes = [
   {
-    path: "/",
-    key: "home",
+    path: '/',
+    key: 'home',
     exact: true,
-    layout: "client",
-    component: loadable(() => import("../pages/home/Home"), {}),
+    layout: 'client',
+    component: loadable(() => import('../pages/home/Home'), {}),
   },
   {
-    path: "/connect",
-    key: "connect",
+    path: '/connect',
+    key: 'connect',
     exact: true,
-    layout: "business",
-    component: loadable(() => import("../pages/connect/Connect"), {}),
+    layout: 'business',
+    component: loadable(() => import('../pages/connect/Connect'), {}),
   },
   {
-    path: "/connect/WalletConnect",
-    key: "WalletConnect",
+    path: '/connect/WalletConnect',
+    key: 'WalletConnect',
     exact: true,
-    layout: "blank",
+    layout: 'blank',
     component: loadable(
-      () => import("../pages/walletconnect/WalletConnect"),
+      () => import('../pages/walletconnect/WalletConnect'),
       {}
     ),
   },
   {
-    path: "/connect/BinanceWallet",
-    key: "BinanceWallet",
+    path: '/connect/BinanceWallet',
+    key: 'BinanceWallet',
     exact: true,
-    layout: "blank",
+    layout: 'blank',
     component: loadable(
-      () => import("../pages/walletbinance/WalletBinance"),
+      () => import('../pages/walletbinance/WalletBinance'),
       {}
     ),
   },
   {
-    path: "/connect/MathWallet",
-    key: "MathWallet",
+    path: '/connect/MathWallet',
+    key: 'MathWallet',
     exact: true,
-    layout: "blank",
-    component: loadable(() => import("../pages/walletmath/WalletMath"), {}),
+    layout: 'blank',
+    component: loadable(() => import('../pages/walletmath/WalletMath'), {}),
   },
   {
-    path: "/connect/SafepalConnect",
-    key: "SafepalConnect",
+    path: '/connect/SafepalConnect',
+    key: 'SafepalConnect',
     exact: true,
-    layout: "blank",
+    layout: 'blank',
     component: loadable(
-      () => import("../pages/walletsafepal/WalletSafepal"),
+      () => import('../pages/walletsafepal/WalletSafepal'),
       {}
     ),
   },
   {
-    path: "/connect/TrustConnect",
-    key: "TrustConnect",
+    path: '/connect/TrustConnect',
+    key: 'TrustConnect',
     exact: true,
-    layout: "blank",
-    component: loadable(() => import("../pages/wallettrust/WalletTrust"), {}),
+    layout: 'blank',
+    component: loadable(() => import('../pages/wallettrust/WalletTrust'), {}),
   },
   {
-    path: "/connect/metamask",
-    key: "MetaMask",
-    layout: "blank",
+    path: '/connect/metamask',
+    key: 'MetaMask',
+    layout: 'blank',
     exact: true,
-    component: loadable(() => import("../pages/walletmetamask/MetaMaskWin"), {
+    component: loadable(() => import('../pages/walletmetamask/MetaMaskWin'), {
       fallback: <MetaMaskLoader />,
     }),
   },
